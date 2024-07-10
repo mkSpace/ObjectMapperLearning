0360d70fd3468b5e74d113c8bbcd52830536d59a
Jaemin.Park <once29@naver.com>
2024-07-10 18:13:19 +0900

    Initialize Project

---
+++ src/main/kotlin/com/funin/objectmapperlearning/ObjectMapperLearningApplication.kt
@@ -0,0 +1,11 @@
+package com.funin.objectmapperlearning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ObjectMapperLearningApplication
+
+fun main(args: Array<String>) {
+    runApplication<ObjectMapperLearningApplication>(*args)
+}
---
+++ src/test/kotlin/com/funin/objectmapperlearning/ObjectMapperLearningApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.funin.objectmapperlearning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ObjectMapperLearningApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
